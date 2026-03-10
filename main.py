0b9fec3312cd304e3143dde608d2548e00224849
Nonashi37 <argeno37@gmail.com>
2026-03-10 21:27:07 +0600

    Complete of Test: Task 1 , 2, 3, 4

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import flet as ft 
 import datetime 
-import os 
+import os  # realizing task 1
 
 def main(page: ft.Page):
     page.title = 'Мое первое приложение На Флет!'
     page.theme_mode = ft.ThemeMode.DARK
 
     greeting_history = []
-
+    favorites = []
 
     if os.path.exists("history.txt"):
         file = open("history.txt", "r", encoding="utf-8")
@@ -16,6 +16,7 @@ def main(page: ft.Page):
         file.close()
 
     history_text = ft.Text(value="History of greetings: \n" + "\n".join(greeting_history))
+    favorites_text = ft.Text(value="Favorite names:\n")
 
     text_hello = ft.Text(value='Как дела')
 
@@ -25,7 +26,7 @@ def main(page: ft.Page):
         if name:
             now = datetime.datetime.now()
             time_string = now.strftime("%Y:%m:%d - %H:%M:%S")
-            
+
             new_message = f"{time_string} - Hello, {name}!"
             
             text_hello.value = f'Приветствую {name}'
@@ -33,11 +34,14 @@ def main(page: ft.Page):
 
             greeting_history.append(new_message)
 
-        
-            file = open("history.txt", "a", encoding="utf-8") # "a" means append
-            file.write(new_message + "\n")
+            # limit history to last 5 realizing Task 4.
+            if len(greeting_history) > 5:
+                greeting_history[:] = greeting_history[-5:]
+
+            file = open("history.txt", "w", encoding="utf-8")
+            for i in greeting_history:
+                file.write(i + "\n")
             file.close()
-        
 
             name_input.value = None
             history_text.value = "History of greetings: \n" + "\n".join(greeting_history)
@@ -61,20 +65,94 @@ def main(page: ft.Page):
     def delete_Button(_):
         greeting_history.clear()
         
-        
         file = open("history.txt", "w", encoding="utf-8")
         file.write("")
         file.close()
-    
 
-        history_text.value = "History of greeting:" 
+        history_text.value = "History of Greeting:" 
         page.update() 
+
+    # morning greetings at realtime if morning add to morning if evening add to evening looking at real time 
+    def show_morning(_):
+        filtered = []
+
+        for line in greeting_history:
+            try:
+                time_part = line.split("-")[1].strip()
+                hour = int(time_part.split(":")[0])
+
+                if hour < 12:
+                    filtered.append(line)
+            except:
+                pass
+
+        history_text.value = "Morning greetings:\n" + "\n".join(filtered)
+        page.update()
+
+    # evening greetings realizing Task 3
+    def show_evening(_):
+        filtered = []
+
+        for line in greeting_history:
+            try:
+                time_part = line.split("-")[1].strip()
+                hour = int(time_part.split(":")[0])
+
+                if hour >= 12:
+                    filtered.append(line)
+            except:
+                pass
+
+        history_text.value = "Evening greetings:\n" + "\n".join(filtered)
+        page.update()
+
+    # add last greeting name to favorites realizing task 2
+    def add_favorite(_):
+        if len(greeting_history) > 0:
+            last = greeting_history[-1]
+
+            try:
+                name = last.split("Hello,")[1]
+                name = name.replace("!", "").strip()
+                favorites.append(name)
+            except:
+                pass
+
+            favorites_text.value = "Favorite names:\n" + "\n".join(favorites)
+
+        page.update()
 
     theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, on_click=edit_theme)
     delete_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=delete_Button)
 
+    morning_button = ft.ElevatedButton("Morning greetings", on_click=show_morning)
+    evening_button = ft.ElevatedButton("Evening greetings", on_click=show_evening)
+
+    favorite_button = ft.ElevatedButton("Add last to favorites", on_click=add_favorite)
+
     main_objects = ft.Row([name_input, elevated_button, theme_button, delete_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
 
-    page.add(text_hello, main_objects, history_text)
+    filter_row = ft.Row([morning_button, evening_button, favorite_button])
+
+    page.add(text_hello, main_objects, filter_row, history_text, favorites_text)
 
 ft.app(main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
